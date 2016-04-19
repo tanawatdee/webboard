131ad4a51896178f4e5fc6b7dc3dd4097d6af1c5
tanawatdee <d.tanawat@hotmail.com>
2016-04-20 00:17:49 +0700

    upnewconsole

--- console.php
+++ console.php
@@ -4,11 +4,6 @@
 	include_once('db/db_profile.php');
 	include_once('db/db_thread.php');
 
-	// print_r(addSth(['table'	=>'thread',
-	// 				'usr'	=>'u4',
-	// 				'thread_topic'=>'topic',
-	// 				'thread_body'=>'body']));
-	print_r(addSth(['table'=>'room',
-					'room_name'=>'room1',
-					'room_des'=>'des']));
+	//new
+
 ?>
